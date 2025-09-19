9f1a376b31a01823ecb7a0e0dd0d18a6988f2815
kskskwi2 <138759675+kskskwi2@users.noreply.github.com>
2025-09-20 01:55:08 +0900

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,6 @@
 @echo off
 set VENV_DIR=venv
 
-REM 가상환경 생성
 if not exist "%VENV_DIR%" (
     echo Creating virtual environment...
     python -m venv %VENV_DIR%
@@ -9,23 +8,22 @@ if not exist "%VENV_DIR%" (
     echo Virtual environment already exists.
 )
 
-REM 가상환경 활성화
-call %VENV_DIR%\Scripts\activate.bat
+call %VENV_DIR%\Scripts\activate
 if ERRORLEVEL 1 (
     echo Failed to activate virtual environment.
     pause
     exit /b 1
 )
 
-REM pip 업그레이드
 python -m pip install --upgrade pip
 
 pip install --upgrade pytubefix pydub pillow
 
-REM 스크립트 실행
-echo Running hoi4_music_generator.py...
-python hoi4_music_generator.py
+echo Running gui.py...
+python gui.py
 
 echo Done!
 pause
+
+
 
